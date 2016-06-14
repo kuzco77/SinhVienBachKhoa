e04fe2aac770228ea349716dbd017fd40d4bd3b0
Chu Nam Anh <chunamanh@itds-macbook-pro.local>
2016-06-14 19:38:54 +0700

    Initial Commit

---
+++ Example/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Example
+//
+//  Created by Chu Nam Anh on 6/14/16.
+//  Copyright © 2016 Chu Nam Anh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
